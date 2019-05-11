2e12a6d0feb18b0797c157dcdfb01e8dfb3c4eaf
Joseph Lyons <josephtlyons@gmail.com>
2019-05-10 22:55:06 -0400

    Move TODO to README.md

--- src/main.rs
+++ src/main.rs
@@ -272,13 +272,3 @@ mod tests {
         assert_eq!(true, main_level.letter_vector.len() == 2);
     }
 }
-
-// TODO
-// Implement Display trait ?
-// Clean up code heavily
-// Run format
-// Remove print debug statements
-// Rename project with a more descriptive name
-// Upload to GitHub, add README.md
-// Have code reviewed and clean up all the non-idiomatic things in here
-// Divide code into modules
