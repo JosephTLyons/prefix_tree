e79daa30ed66e7304a5e7e24dfbee70eacad24f9
Joseph Lyons <josephtlyons@gmail.com>
2019-05-13 05:08:14 -0400

    Don't need to use match for this case

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,21 @@ use std::io::{BufRead, BufReader};
 fn main() {
     let mut actual_letter_count: usize = 0;
 
-    match File::open("../word_files/my_words.txt") {
-        Ok(words_file) => {
-            let buff = BufReader::new(words_file);
-            let mut pt: PrefixTree = PrefixTree::new();
+    let words_file =
+        File::open("../word_files/my_words.txt").expect("File could not be opened: {}");
 
-            for line in buff.lines() {
-                let result = line.unwrap();
-                pt.insert_word(&result);
-                actual_letter_count += result.len();
-            }
+    let buff = BufReader::new(words_file);
+    let mut pt: PrefixTree = PrefixTree::new();
 
-            pt.print_all_words();
-            println!("Letters in file: {}", actual_letter_count);
-            println!("Letters in tree: {}", pt.get_letter_count());
-            println!("Words in tree:   {}", pt.get_word_count());
-        }
-
-        Err(e) => println!("File could not be opened: {}", e),
+    for line in buff.lines() {
+        let result = line.unwrap();
+        pt.insert_word(&result);
+        actual_letter_count += result.len();
     }
+
+    pt.print_all_words();
+
+    println!("Letters in file: {}", actual_letter_count);
+    println!("Letters in tree: {}", pt.get_letter_count());
+    println!("Words in tree:   {}", pt.get_word_count());
 }
